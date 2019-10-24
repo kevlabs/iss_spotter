1d67733865882220462767cf169c4693745f5d47
Kevin Laburte <kevinlaburte@Kevins-MacBook-Pro-2.local>
2019-10-24 16:16:40 -0400

    Implemented ISS 3

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const {fetchMyIP, fetchCoordsByIP} = require('./iss');
+const {fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes} = require('./iss');
 
 // fetchMyIP((err, ip) => console.log(err ? err : ip));
 // fetchCoordsByIP('sadasd', (err, coord) => console.log(err ? err : coord));
+// fetchISSFlyOverTimes({latitude: '43.63830', longitude: '-79.43010'}, (err, times) => console.log(err ? err : times));
 fetchMyIP((err, ip) => {
   if (err) {
     console.log(err);
   } else {
-    fetchCoordsByIP(ip, (err, coord) => console.log(err ? err : coord));
+    fetchCoordsByIP(ip, (err, coord) => {
+      if (err) {
+        console.log(err);
+      } else {
+        fetchISSFlyOverTimes(coord, (err, times) => console.log(err ? err : times));
+      }
+    });
   }
 });
--- iss.js
+++ iss.js
@@ -43,6 +43,34 @@ const fetchCoordsByIP = function(ip, callback) {
     }
 
   });
-}
+};
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+const fetchISSFlyOverTimes = function({latitude, longitude}, callback) {
+  request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`, (err, response, body) => {
+    if (err) {
+      callback(err, null);
+    } else if (!response || response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS flyover times. Response: ${body}`), null);
+    } else {
+      const {response: times} = JSON.parse(body);
+      if (!times || !times.length) {
+        callback(Error('Cannot get ISS flyover times'), null);
+      } else {
+        callback(null, times);
+      }
+    }
+
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
